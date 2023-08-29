b5b6f9922210d51c41f11ef39f6257b1b881a593
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-08-29 12:52:16 +0530

    thinker

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("kill me")
+import tkinter as tk
+
+def change_color():
+    color = color_entry.get()
+    window.configure(bg=color)
+
+window = tk.Tk()
+window.title("Change Window Color")
+
+# Create a label and entry field for the color input
+color_label = tk.Label(window, text="Enter color:")
+color_label.pack()
+color_entry = tk.Entry(window)
+color_entry.pack()
+
+# Create a button to change the color
+change_button = tk.Button(window, text="Change Color", command=change_color)
+change_button.pack()
+
+window.mainloop()
