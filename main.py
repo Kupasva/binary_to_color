280c1aa28afb9e242f425c9d2d864fdb25fa8293
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-09-07 19:30:39 +0530

    clicked widget in PyQt6

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.button_is_checked = True
+
         self.setWindowTitle("My App")
 
         button = QPushButton("Press me!")
-        button.setChecked(True)
-        button.clicked.connect(self.the_button_was_clicked)
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
+
 
         # set the central widget of the window
         self.setCentralWidget(button)
-    def the_button_was_clicked(self):
-        print("Clicked")
+    # def the_button_was_clicked(self):
+    #     print("Clicked!")
+
+    def the_button_was_toggled(self,checked):
+        self.button_is_checked = checked
+        print(self.button_is_checked)
 
 app = QApplication(sys.argv)
 
